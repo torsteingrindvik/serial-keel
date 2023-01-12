96b8a099785ec51a7ebb2a5ed335a2dc6f18cb3b
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2023-01-12 16:49:32 +0100

    Bare minimum pane
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- frontend/iced/src/pane.rs
+++ frontend/iced/src/pane.rs
@@ -1,24 +1,46 @@
+use iced::theme::{self, Button};
+use iced::widget::{container, pane_grid, row, scrollable, text, PaneGrid};
 use iced::{widget::Text, Element};
+use iced::{Color, Length};
+use iced_lazy::responsive;
 
 use crate::{Icon, Message, Tab};
 
-pub struct PaneTab {}
+pub struct PaneTab {
+    panes: pane_grid::State<Pane>,
+    focus: Option<pane_grid::Pane>,
+}
+
+struct Pane {
+    id: usize,
+}
+
+impl Pane {
+    fn new(id: usize) -> Self {
+        Self { id }
+    }
+}
 
 impl PaneTab {
     pub fn new() -> Self {
-        Self {}
+        let (panes, _) = pane_grid::State::new(Pane::new(0));
+
+        Self { focus: None, panes }
     }
 
     pub fn update(&mut self, message: PaneMessage) {
         match message {
-            PaneMessage::Click => {}
+            PaneMessage::Clicked(_) => {
+                println!("CLICKLY")
+            }
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum PaneMessage {
-    Click,
+    // Inner(pane_grid::)
+    Clicked(pane_grid::Pane),
 }
 
 impl Tab for PaneTab {
@@ -33,8 +55,52 @@ impl Tab for PaneTab {
     }
 
     fn content(&self) -> Element<Message> {
-        let content: Element<PaneMessage> = Text::new("Pane Page").size(50).into();
+        let pane_grid: Element<PaneMessage> =
+            PaneGrid::new(&self.panes, |id, pane, is_maximized| {
+                let is_focused = self.focus == Some(id);
 
-        content.map(Message::Pane)
+                let title = row![
+                    "Pane",
+                    text(pane.id.to_string()).style(if is_focused {
+                        PANE_ID_COLOR_FOCUSED
+                    } else {
+                        PANE_ID_COLOR_UNFOCUSED
+                    }),
+                ];
+
+                let title_bar = pane_grid::TitleBar::new(title);
+
+                pane_grid::Content::new(responsive(move |size| view_content(id)))
+                    .title_bar(title_bar)
+            })
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .on_click(PaneMessage::Clicked)
+            .into();
+
+        container(pane_grid.map(Message::Pane))
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 }
+
+fn view_content<'a>(pane: pane_grid::Pane) -> Element<'a, PaneMessage> {
+    let content = text("Pane Content");
+
+    container(scrollable(content))
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .into()
+}
+
+const PANE_ID_COLOR_UNFOCUSED: Color = Color::from_rgb(
+    0xFF as f32 / 255.0,
+    0xC7 as f32 / 255.0,
+    0xC7 as f32 / 255.0,
+);
+const PANE_ID_COLOR_FOCUSED: Color = Color::from_rgb(
+    0xFF as f32 / 255.0,
+    0x47 as f32 / 255.0,
+    0x47 as f32 / 255.0,
+);
