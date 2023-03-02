1aa4203845d46a93987a5c728de8138af2a2dbb2
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2023-03-02 12:00:40 +0100

    Add precompiled version
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- build-musl.sh
+++ build-musl.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cargo build --release --target x86_64-unknown-linux-musl
+cargo build --release --bin serial-keel --target x86_64-unknown-linux-musl --features mocks-share-endpoints
 cp target/x86_64-unknown-linux-musl/release/serial-keel bin/serial-keel
+
