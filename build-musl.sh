a156cfebb7c4fe65b247821fd4a407690165ddca
Torstein Grindvik <52322338+torsteingrindvik@users.noreply.github.com>
2023-03-27 13:46:17 +0200

    Make mocks-share-endpoints a default feature (#32)
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- build-musl.sh
+++ build-musl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cargo build --release --bin serial-keel --target x86_64-unknown-linux-musl --features mocks-share-endpoints
+cargo build --release --bin serial-keel --target x86_64-unknown-linux-musl
 cp target/x86_64-unknown-linux-musl/release/serial-keel bin/serial-keel
 
